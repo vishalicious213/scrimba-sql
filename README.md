7f8e1ff2b2441f17daa64c2e7b0613f081cbb94b
vishalicious213 <vishalicious213@gmail.com>
2025-12-30 16:57:23 -0800

    SELECT brand, model, color, price FROM cars WHERE color = 'black'

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Scrimba Intro to SQL
+
+- brand, model, year, price, color, condition, sold
+
+## Commands used per lesson
+
+- SELECT * FROM cars;
+- SELECT brand, model, price FROM cars;
+- SELECT brand, model, condition, year FROM cars;
--- query.sql
+++ query.sql
@@ -1,3 +1,8 @@
 -- SELECT * FROM cars;
+
 -- SELECT brand, model, price FROM cars;
-SELECT brand, model, condition, year FROM cars;
+
+-- SELECT brand, model, condition, year FROM cars;
+
+SELECT brand, model, color, price FROM cars
+WHERE color = 'black';
