0dcb15b4ddc583265c7ed8b281175ea732b65560
vishalicious213 <vishalicious213@gmail.com>
2026-01-03 15:51:29 -0800

    added new queries & updated README

--- challenges-1.sql
+++ challenges-1.sql
@@ -1,4 +1,9 @@
+-- SELECT brand, model, color FROM cars
+-- WHERE color LIKE '%red%'
+-- AND brand != 'Ferrari'
+-- AND sold = false;
+
 SELECT brand, model, color FROM cars
-WHERE color LIKE '%red%'
-AND brand != 'Ferrari'
+WHERE color NOT IN ('red', 'blue', 'white')
+AND brand NOT IN ('Aston Martin', 'Bentley', 'Jaguar')
 AND sold = false;
