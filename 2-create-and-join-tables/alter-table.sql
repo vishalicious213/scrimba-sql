0c7f86ba978ccf764d0c9f88865c21c240b41723
vishalicious213 <vishalicious213@gmail.com>
2026-01-09 15:44:01 -0800

    drafted /2 alter-table.sql

---
+++ 2-create-and-join-tables/alter-table.sql
@@ -0,0 +1,12 @@
+-- 1. Add the column (initially nullable)
+ALTER TABLE cars
+ADD COLUMN dealership_id INTEGER;
+
+-- 2. Insert data to backfill the dealership_id column
+
+
+-- 3. Add the NOT NULL constraint
+
+
+-- 4. Add the foreign key constraint
+
