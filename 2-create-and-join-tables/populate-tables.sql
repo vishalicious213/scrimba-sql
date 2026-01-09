46cbc9606a936fe4891ff8a8de6c75b96ba9bdb7
vishalicious213 <vishalicious213@gmail.com>
2026-01-09 13:25:46 -0800

    drafted /2 populate-tables.sql

---
+++ 2-create-and-join-tables/populate-tables.sql
@@ -0,0 +1,12 @@
+/*
+	Add the following data to the dealerships table:
+	
+	| city         | state | established |
+	| ------------ | ----- | ----------- |
+	| Chicago      | IL    | 2022-04-14  |
+	| Atlanta      | GA    | 2022-04-14  |
+	| Detroit      | MI    | 2025-05-01  |
+	| Philadelphia | PA    | 2026-07-01  |
+	
+	Note: dates are in YYYY-MM-DD format
+*/
