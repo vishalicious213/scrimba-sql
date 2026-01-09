e96b812c6b25b8fb344ac8c68b174443f7b40618
vishalicious213 <vishalicious213@gmail.com>
2026-01-09 13:18:02 -0800

    updated /2 create-tables.sql & README.md

--- 2-create-and-join-tables/create-tables.sql
+++ 2-create-and-join-tables/create-tables.sql
@@ -54,3 +54,21 @@ CREATE TABLE IF NOT EXISTS staff (
     name TEXT NOT NULL,
     role TEXT NOT NULL
 );
+
+/*
+	Create the sold_cars table
+		Include these columns:
+			id - serial primary key - NOT NULL
+			cars_id - integer - NOT NULL - foreign key referencing cars(id)
+			seller - integer - NOT NULL - foreign key referencing staff(id)
+			sold_date DATE - NOT NULL
+			sold_price INTEGER - NOT NULL
+*/
+
+CREATE TABLE IF NOT EXISTS sold_cars (
+    id SERIAL PRIMARY KEY,
+    cars_id INTEGER NOT NULL REFERENCES cars(id),
+    seller INTEGER NOT NULL REFERENCES staff(id),
+    sold_date DATE NOT NULL,
+    sold_price INTEGER NOT NULL
+);
